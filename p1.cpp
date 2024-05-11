0c8c4dc97a1d9c01acba80a44e3e5ed7208aeace
bo4karev <54407871+bo4karev@users.noreply.github.com>
2024-05-11 22:34:16 +0300

    1

--- p1.cpp
+++ p1.cpp
@@ -14,6 +14,9 @@ if ( a > b ) x = a;
 
 cout << "1. -sin(x-5) = " << -sin (x-5) << endl;
 cout << "2. ln(x^2) = " << log(x^2) << endl;
+cout << "3. sin(x-5) = " << sin(x-5) << endl;
+cout << "4. 2^x = " << (2^x) << endl;
+cout << "5. cos(x+5)^2 = " << cos(x+5)*cos(x+5)<< endl;
 
 return 0;
 }
